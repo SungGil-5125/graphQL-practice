935f3d92e099980b607f5fc81072f67668f0d664
김성길 <82089918+SungGil-5125@users.noreply.github.com>
2022-10-20 17:18:40 +0900

    🎉 :: project setting

---
+++ src/main/java/com/project/graphQLpractice/GraphQlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.project.graphQLpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/graphQLpractice/GraphQlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.graphQLpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
